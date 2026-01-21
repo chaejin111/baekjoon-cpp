939fc6a9d87d99cbc1fda25c34de6573d222071c
chaejin111 <chaejin248@gamil.com>
2026-01-21 22:47:01 +0900

    init: start baekjoon cpp repository

---
+++ 11022.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   int T,a,b,i;
+   i = 1;
+   cin >> T;
+   while (true) {
+	   if (i > T) {
+		   break;
+	   }
+	   cin >> a >> b;
+	   cout << "Case #" << i << ": " << a << " + " << b << " = " <<  a + b << "\n";
+	   i++; 
+   }
+   return 0;
+}
---
+++ 2438.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	for(int line = 1;line <= T;line++){
+	for (int i = 0; i < line; i++) {
+		cout << "*";
+	 }
+	 cout << "\n";
+	}
+	
+	return 0;
+}
