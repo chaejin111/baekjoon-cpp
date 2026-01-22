421b808e98d6e852a368c14a4dc27761633d1197
chaejin111 <chaejin248@gamil.com>
2026-01-22 20:22:35 +0900

    solve: BOJ 2562

---
+++ 2562.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N[9];
+	for (int i = 0; i < 9; i++) {
+		cin >> N[i];
+	}
+	int Max_count = 1;
+	int Max_N = N[0];
+	for (int i = 1; i < 9; i++) {
+		if (Max_N < N[i]) {
+			Max_N = N[i];
+			Max_count = i+1;
+		}
+	}
+	cout << Max_N << " " << Max_count;
+
+
+	return 0;
+}
