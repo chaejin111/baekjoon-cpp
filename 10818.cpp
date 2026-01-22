9a27f1aad2e5051fe0509c6b4aff45b44a916e50
chaejin111 <chaejin248@gamil.com>
2026-01-22 20:04:36 +0900

    solve: BOJ 10818

---
+++ 10818.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x,N,Max_x,Min_x;
+	cin >> N;
+	cin >> x;
+	Max_x = x;
+	Min_x = x;
+	for (int i = 1; i < N; i++) {
+		cin >> x;
+		if (Max_x < x) {
+			Max_x = x;
+		}
+		if (Min_x > x) {
+			Min_x = x;
+		}
+	}
+	cout << Min_x << " " << Max_x;
+	return 0;
+}
