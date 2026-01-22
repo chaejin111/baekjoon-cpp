43642be00d429918f87b38d75d15c819a16d6de3
chaejin111 <chaejin248@gamil.com>
2026-01-22 19:31:00 +0900

    solve: BOJ 10871

---
+++ 10871.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, X, a;
+	cin >> N >> X;
+	int A[10000];
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		A[i] = a;
+	}
+	for (int i = 0; i < N; i++) {
+		if (A[i] < X) {
+			cout << A[i] << " ";
+		}
+	}
+	return 0;
+}
