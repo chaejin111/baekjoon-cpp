bff8d3452f9940b6effa621275bff79e3a13110e
chaejin111 <chaejin248@gamil.com>
2026-01-24 00:19:45 +0900

    solve: BOJ 27866

---
+++ 27866.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string S;
+	int i;
+	getline(cin,S);
+	cin >> i;
+	i--;
+	cout << S.at(i);
+
+	return 0;
+}
