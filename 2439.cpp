12b46dbffd58ddc5b914511e6d20ef3ca9fedf14
chaejin111 <chaejin248@gamil.com>
2026-01-23 23:31:10 +0900

    solve: BOJ 2439

---
+++ 2439.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N,blank;
+	cin >> N;
+	blank = N-1;
+	for (int i = 0; i < N; i++) {
+		for (int i2 = 0; i2 < blank; i2++) {
+			cout << " ";
+		}
+		for (int i3 = 0; i3 < N - blank; i3++) {
+			cout << "*";
+		}
+		blank--;
+		cout << "\n";
+	}
+	return 0;
+	
+}
