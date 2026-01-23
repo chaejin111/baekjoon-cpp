bb306af647bf97f3f4d0581e86da6a8efeee16eb
chaejin111 <chaejin248@gamil.com>
2026-01-24 00:41:56 +0900

    solve: BOJ 9086

---
+++ 9086.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string Line;
+	getline(cin,Line);
+	int T = stoi(Line);
+	for(int idx = 0; idx<T; idx++){
+		string S;
+		getline(cin, S);
+		cout << S[0] << S[S.size() - 1] << "\n";
+	}
+	return 0;
+}
