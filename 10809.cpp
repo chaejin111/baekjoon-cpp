7cb447bf9d737ba023a85bb897ac67e7126bb8f8
chaejin111 <chaejin248@gamil.com>
2026-01-25 20:37:35 +0900

    solve: BOJ 10809

---
+++ 10809.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string S;
+	int AlphabetS[26];
+	cin >> S;
+	fill(AlphabetS, AlphabetS+26,-1);
+	for (int i = 0; i < S.length(); i++) {
+		int a = S[i] - 'a';
+		if(AlphabetS[a] == -1)
+		{
+		AlphabetS[a] = i;
+		}
+
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << AlphabetS[i] << " ";
+	}
+}
