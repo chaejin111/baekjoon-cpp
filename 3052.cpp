33f86ecbe76b1c617d84f1aa43bd7d18aa93915e
chaejin111 <chaejin248@gamil.com>
2026-01-25 20:38:56 +0900

    solve: BOJ 3052

---
+++ 3052.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int mod[10];
+	int arr[42] = {0};
+	int a = 0;
+	for (int i = 0; i < 10; i++) {
+		int n;
+		cin >> n;
+		mod[i] = n % 42;
+	}
+	for (int i = 0; i < 10; i++) {
+		arr[mod[i]] = 1;
+	}
+	for (int i = 0; i < 42; i++) {
+		if (arr[i] == 1) {
+			a++;
+		}
+	}
+	cout << a;
+	return 0;
+}
