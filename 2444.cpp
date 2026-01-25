34bea6d95aac3c2bda7bfc9b900ef5a63a1d4a73
chaejin111 <chaejin248@gamil.com>
2026-01-25 23:39:49 +0900

    solve: BOJ 2444

---
+++ 2444.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < (2 * N) - 1; i++) {
+		int space = (i < N) ? (N-1-i) : (i - (N - 1));
+		int stars = (i<N) ? (2*i+1): (2 * (2 * N - 2 - i) + 1);
+		for (int s = 0; s < space; s++) cout << ' ';
+		for (int k = 0; k < stars; k++)  cout << '*';
+		cout << '\n';
+	}
+	return 0;
+}
