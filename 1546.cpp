d807754d717af46e737257d0325e91c96732a20b
chaejin111 <chaejin248@gamil.com>
2026-01-25 23:40:18 +0900

    solve: BOJ 1546

---
+++ 1546.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	double avg[1000];
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int score;
+		cin >> score;
+		avg[i] = score;
+	}
+	double maxscore = avg[0];
+	for (int i = 1; i < N; i++) {
+		if (maxscore < avg[i]) {
+			maxscore = avg[i];
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		avg[i] = (avg[i]/maxscore)*100;
+	}
+	double sumscore = 0;
+	for (int i = 0; i < N; i++) {
+		sumscore += avg[i];
+	}
+	cout << sumscore/N;
+	return 0;
+}
