03628b5827ad7dbaa329f7e5eecab8f4cc021967
chaejin111 <chaejin248@gamil.com>
2026-01-25 20:37:57 +0900

    solve: BOJ 10811

---
+++ 10811.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N,M = 0;
+	cin >> N >> M;
+	int A[101];
+	for (int i = 0; i <= N; i++) {
+		A[i] = i;
+	}
+	for (int idx = 0; idx < M; idx++) {
+		int i,j;
+		cin >> i >> j;
+		while (i < j) {
+			int changen = A[i];
+			A[i] = A[j];
+			A[j] = changen;
+			i++;
+			j--;
+		}
+	}
+     for (int idx = 1; idx <= N; idx++) {
+		 cout << A[idx] << " ";
+	}
+	return 0;
+}
