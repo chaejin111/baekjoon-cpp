543df7b608673d24e2b88070c119071ad29d20a8
chaejin111 <chaejin248@gamil.com>
2026-01-25 20:38:31 +0900

    solve: BOJ 11720

---
+++ 11720.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N,sumN = 0;
+	string s;
+	cin >> N;
+	cin >> s;
+
+	for (int idx = 0; idx < N; idx++) {
+		sumN += s[idx] - '0';
+	}
+	cout << sumN;
+	return 0;
+}
