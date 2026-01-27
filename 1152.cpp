f35465edac527cd214552e35e572efb0a1996b1a
chaejin111 <chaejin248@gamil.com>
2026-01-27 19:06:04 +0900

    solve BOJ: 1152

---
+++ 1152.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string A;
+	int word = 0;
+	getline(cin,A);
+	bool inword = false;
+
+	for (char c : A) {
+		if (c != ' ') {
+			if (!inword) {
+				word++;
+				inword = true;
+			}
+		}
+		else inword = false;
+	}
+	cout << word;
+	return 0;
+}
