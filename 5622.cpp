985b8197c78504673d92dbb6cdd7366662a9ed95
chaejin111 <chaejin248@gamil.com>
2026-01-28 18:31:09 +0900

    solve: BOJ 5622

---
+++ 5622.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int time = 0;
+	string s;
+	cin >> s;
+	for (char c : s) {
+		if(c <= 'C') {time+=3;}
+		else if (c <= 'F') { time += 4; }
+		else if (c <= 'I') { time += 5; }
+		else if (c <= 'L') { time += 6; }
+		else if (c <= 'O') { time += 7; }
+		else if (c <= 'S') { time += 8; }
+		else if (c <= 'V') { time += 9; }
+		else { time += 10; }
+	}
+	cout << time;
+	return 0;
+}
