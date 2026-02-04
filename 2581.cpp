65c0aad7b99f4b2c90ed77417eeedae4f19879a7
chaejin111 <chaejin248@gamil.com>
2026-02-04 23:13:21 +0900

    solve: BOJ 2581 9506

---
+++ 2581.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    int N,M,min = 0,sum = 0;
+    cin >> N >> M;
+    for (int i = N; i <= M; i++) {
+        bool ismeasure = true;
+        if (i < 2) {
+            continue;
+        }
+        for (int idx = 2; idx*idx <= i; idx++) {
+            if (i % idx == 0) {
+                ismeasure = false;
+                break;
+            }
+        }
+        if (ismeasure) {
+            if (min == 0) {
+                min = i;
+            }
+            sum +=i;
+        }
+    }
+    if(min == 0){cout << -1;}
+    else cout << sum << '\n' << min;
+    return 0;
+}
---
+++ 9506.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    while (true) {
+        int N,sum = 0;
+        vector<int> measure;
+        cin >> N;
+        if (N == -1) {
+            break;
+        }
+        for (int i = 1; i < N; i++) {
+            if (N % i == 0) {
+                measure.push_back(i);
+                sum += i;
+            }
+        }
+        if (sum == N) {
+            cout << N << " = ";
+            for (int i = 0; i < measure.size(); i++) {
+                if (i == measure.size() - 1) {
+                    cout << measure[i] << '\n';
+                }
+                else {cout << measure[i] << " + ";}
+            }
+        }
+        else {cout << N << " is NOT perfect." << '\n'; }
+    }
+    return 0;
+}
